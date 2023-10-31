f25d56fca4263c955da9cf632d831d26b48e3d2f
tsouris <nikolastsouris16@gmail.com>
2023-10-31 20:44:36 +0200

    Home Page

--- Views/HomeView.xaml.cs
+++ Views/HomeView.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using Project_BlueLock.Utilities;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace Project_BlueLock.Views
 {
@@ -10,6 +16,55 @@ namespace Project_BlueLock.Views
         public HomeView()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        //private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    WindowInteropHelper helper = new WindowInteropHelper(this);
+        //    SendMessage(helper.Handle, 161, 2, 0);
+        //}
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+
+            if (parentWindow != null)
+            {
+                if (parentWindow.WindowState == WindowState.Maximized)
+                {
+                    parentWindow.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    parentWindow.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+
+            if (parentWindow != null)
+            {
+                parentWindow.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
