ec9f67387765c23007406b55d9bf7eecaf8c0bc5
DennisTsouris <122720690+DennisTsouris@users.noreply.github.com>
2023-11-02 20:11:12 +0200

    Finished PlayerProfileView

--- Views/PlayerProfileView.xaml.cs
+++ Views/PlayerProfileView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Project_BlueLock.Views
 {
@@ -11,5 +12,43 @@ namespace Project_BlueLock.Views
         {
             InitializeComponent();
         }
+
+        private void tbName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void tbSurname_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void tbUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+
+            if (parentWindow != null)
+            {
+                if (parentWindow.WindowState == WindowState.Maximized)
+                {
+                    parentWindow.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    parentWindow.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+
+        private void tbDescription_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
     }
 }
