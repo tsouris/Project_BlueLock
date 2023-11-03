150bbaddaafbbb41abc56274c34bd4075ad9c954
tsouris <nikolastsouris16@gmail.com>
2023-11-03 17:38:29 +0200

    Worked a little more on the home content

--- Views/HomeContentView.xaml.cs
+++ Views/HomeContentView.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Project_BlueLock.Views
 {
@@ -23,6 +12,21 @@ namespace Project_BlueLock.Views
         public HomeContentView()
         {
             InitializeComponent();
+        }
+
+        private bool isPlaying = false;
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                mediaElement.Pause();
+                isPlaying = false;
+            }
+            else
+            {
+                mediaElement.Play();
+                isPlaying = true;
+            }
         }
     }
 }
